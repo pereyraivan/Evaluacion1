7478ae81ab20704222df29a2a3c58a9629098cbc
Ivan <pereyraivan@hotmail.com>
2023-09-25 15:55:08 -0300

    Agrego controlador GetPerson

---
+++ Evaluaion1/Evaluaion1/Controllers/PersonController.cs
@@ -0,0 +1,21 @@
+﻿using Evaluaion1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Evaluaion1.Controllers
+{
+    
+    public class PersonController : Controller
+    {
+        public IActionResult GetPerson()
+        {
+            var person = new PersonModel
+            {
+                Nombre = "Juan",
+                Apellido = "Perez",
+                Edad = 33
+            };
+            
+            return View("Index", person);
+        }
+    }
+}
---
+++ Evaluaion1/Evaluaion1/Models/PersonModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Evaluaion1.Models
+{
+    public class PersonModel
+    {
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public int? Edad { get; set; }
+    }
+}
